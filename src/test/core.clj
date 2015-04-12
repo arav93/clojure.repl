1a5e9440103cca7993d1135f336e17d2a6cee803
Aravind K N <aravikn@gmail.com>
2015-04-12 22:48:27 +0530

    Update core.clj
    Altered annotation for apropos

--- src/test/core.clj
+++ src/test/core.clj
@@ -1,7 +1,7 @@
 (ns test.core 
 (:require [clojure.core.typed :as t]))
 
-(t/ann ^:no-check clojure.repl/apropos [String -> clojure.lang.LazySeq])
+(t/ann ^:no-check clojure.repl/apropos [String -> (clojure.lang.LazySeq String)])
 (t/ann ^:no-check clojure.repl/demunge [String -> String])
 (t/ann ^:no-check clojure.repl/source-fn [clojure.lang.Symbol -> String])
 
